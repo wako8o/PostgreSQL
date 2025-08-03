4043a47f58f80ec8c1dc6036ccfc2ce1d1d3d814
wako8o <emilaleksandrov11@gmail.com>
2025-08-03 10:16:11 +0200

    new

---
+++ 04_Basic CRUD - Exercise/06. Find the Addresses.sql
@@ -0,0 +1,6 @@
+SELECT
+    id,
+   concat_ws(' ', number, street) AS address,
+   city_id
+FROM addresses
+WHERE id >= 20
---
+++ 04_Basic CRUD - Exercise/07. Positive Even Number.sql
@@ -0,0 +1,7 @@
+SELECT
+   concat_ws(' ', number, street) AS address,
+   city_id
+FROM addresses
+WHERE city_id % 2 = 0
+ORDER BY city_id
+;
